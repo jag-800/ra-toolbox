931c298d791d208fd4570e9d79aa8565b0cedd00
Yunji Jo <joyunji1119@gmail.com>
2023-06-27 17:34:34 +0900

    test

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,7 +1,7 @@
-import "./custom/jp_content";
-import "./custom/en_content";
-import "./custom/new";
-import "./custom/translation";
+// import "./custom/jp_content";
+// import "./custom/en_content";
+// import "./custom/new";
+// import "./custom/translation";
 
 //= require jquery
 //= require jquery_ujs
